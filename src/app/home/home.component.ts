e72c63a0b45b86c47adbd1ad0fb859bd880bdf3d
PriyaChodankar46 <priyachodankar46@gmail.com>
2021-06-13 12:48:28 +0530

    Add holiday

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CalendarEvent, CalendarView } from 'angular-calendar';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import * as data from '../../assets/HolidayList.json';
 import { Item } from '../model/item';
 
+//https://getbootstrap.com/docs/4.3/components/modal/
+//https://angular-calendar.com/#/kitchen-sink
 @Component({
   selector: 'ngbd-modal-content',
   template: `
-    <div class="modal-header"></div>
-    <div class="modal-body" [innerHTML]="message"></div>
+    <div class="modal-header">
+      <h4>Holiday</h4>
+    </div>
+    <div class="modal-body">
+      <div class="col-md-12" style="border: thin solid #000000;padding: 10px;" 
+      [innerHTML]="message"></div>
+      <br>
+      <div class="col-md-12" style="border: thin solid #000000;padding: 10px;">
+        <h5>Add new holiday</h5>
+        <div class="col-md-12">
+          <label for="Holiday-Name" class="col-form-label">Holiday Name</label>
+        </div>
+        <div class="col-md-12" style="padding-bottom: 10px;">
+          <input type="text" class="form-control" #holidayName />
+        </div>
+        <div class="col-md-12" style="">
+        <button
+          type="button"
+          class="btn btn-outline-dark"
+          (click)="addHoliday(holidayName.value)"
+        >
+          Add Holiday
+        </button>
+        </div>
+      </div>
+    </div>
     <div class="modal-footer">
       <button
         type="button"
@@ -22,8 +48,11 @@ import { Item } from '../model/item';
 })
 export class NgbdModalContent {
   @Input() message: string = '';
-
+  @Output() holidayMsg = new EventEmitter();
   constructor(public activeModal: NgbActiveModal) {}
+  addHoliday(holidayName: string) {
+    this.holidayMsg.emit(holidayName);
+  }
 }
 @Component({
   selector: 'app-home',
@@ -48,6 +77,13 @@ export class HomeComponent implements OnInit {
     obj.forEach((element) => {
       this.dates.push(element);
     });
+    if (localStorage.getItem('datesList') == null) {
+      localStorage.setItem('datesList', JSON.stringify(this.dates));
+    }
+    this.setEvents();
+  }
+
+  setEvents() {
     for (let i = 0; i < this.dates.length; i++) {
       this.items.push({
         title: this.dates[i].name,
@@ -57,7 +93,6 @@ export class HomeComponent implements OnInit {
     }
     this.events = this.items;
   }
-
   //https://angular-calendar.com/#/clickable-events
   dayClicked({ date, events }: { date: Date; events: CalendarEvent[] }): void {
     var eventsMessage = '';
@@ -66,11 +101,30 @@ export class HomeComponent implements OnInit {
       eventsMessage = eventsMessage + element.title;
       eventsMessage += '</p>';
     });
-    if (eventsMessage != '') {
-      //https://ng-bootstrap.github.io/#/components/modal/examples
-      const modalRef = this.modalService.open(NgbdModalContent);
-      modalRef.componentInstance.message = eventsMessage;
+    if (eventsMessage == '') {
+      eventsMessage = 'No holidays today.';
     }
+    //https://ng-bootstrap.github.io/#/components/modal/examples
+    const modalRef = this.modalService.open(NgbdModalContent);
+    modalRef.componentInstance.message = eventsMessage;
+    //https://stackoverflow.com/questions/42681556/event-emitter-from-bootstrap-modal-to-parent
+    modalRef.componentInstance.holidayMsg.subscribe((value: any) => {
+      var obj = {
+        date: date,
+        name: value,
+      };
+      this.dates.push(obj);
+      localStorage.setItem('datesList', JSON.stringify(this.dates));
+      this.events = [
+        ...this.events,
+        {
+          title: obj.name,
+          start: obj.date,
+          color: { primary: '#e3bc08', secondary: '#FDF1BA' },
+        },
+      ];
+      modalRef.close();
+    });
     //this.openAppointmentList(date)
   }
 }
